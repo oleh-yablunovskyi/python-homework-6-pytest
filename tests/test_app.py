cfb4100077e1045c32cb12b424ed10d2ae2d4924
Oleh Yablunovskyi <lifserv@gmail.com>
2025-12-11 23:36:07 +0200

    test: Add parametrized tests for is_even func

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,17 @@ import pytest
 
 from app import add, is_even
 
+
 def test_add_simple():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
+
+@pytest.mark.parametrize("value, expected", [
+    (2, True),
+    (3, False),
+    (10, True),
+])
+def test_is_even(value, expected):
+    assert is_even(value) == expected
